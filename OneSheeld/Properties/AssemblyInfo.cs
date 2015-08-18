83676bf83a71c4248a807c9e5066d898d3aef823
Jeff French <arduinocodedog@gmail.com>
2015-08-18 15:48:25 -0500

    v1.6 (project version updates)
    Testing so far is going pretty well.  Haven't found any major gotcha's
    yet.  These changes just bring the version number of all projects to
    v1.6.   This won't change how things work, or even matter to most.  But
    it's something I've been trying to do on recent releases.

--- OneSheeld/Properties/AssemblyInfo.cs
+++ OneSheeld/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OneSheeld Library")]
-[assembly: AssemblyDescription("OneSheeld Library for Netduino v1.5")]
+[assembly: AssemblyDescription("OneSheeld Library for Netduino v1.6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OneSheeld Library")]
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
--- SimpleGLCD/Properties/AssemblyInfo.cs
+++ SimpleGLCD/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleGLCD")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SimpleGLCD 1Sheeld Sample for Netduino")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleGLCD")]
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
